2d0ac993da14d648e694ec439576fbd8a82ad8d6
skpk <stefan.freespace@gmail.com>
2019-08-05 09:58:34 +0200

    Playground with estimate size

--- tests/test_downloader/test_models__estimate_size.py
+++ tests/test_downloader/test_models__estimate_size.py
@@ -26,6 +26,7 @@ class DownloadRequestEstimateSizeTestCase(TestCase):
                     'is_enum': False,
                     'distribution': None,
                 },
+
             ],
         )
 
