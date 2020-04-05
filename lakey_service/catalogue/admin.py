27a836212d994676bba78aa9c0ef5197dbe530c5
slomeks <maciekslomka@gmail.com>
2020-04-06 00:01:44 +0200

    inject custom messages as placeholders

--- lakey_service/catalogue/admin.py
+++ lakey_service/catalogue/admin.py
@@ -3,11 +3,6 @@ from django.contrib import admin
 from .models import CatalogueItem
 
 
+@admin.register(CatalogueItem)
 class CatalogueItemAdmin(admin.ModelAdmin):
-
-    model = CatalogueItem
-
-    list_display = ('name',)
-
-
-admin.site.register(CatalogueItem, CatalogueItemAdmin)
+    list_display = ('name', 'executor_type', 'maintained_by')
--- lakey_service/conf/settings.py
+++ lakey_service/conf/settings.py
@@ -140,9 +140,9 @@ INSTALLED_APPS = (
 
     'django.contrib.postgres',
     'django.contrib.auth',
-    # 'django.contrib.admin',
-    'material.admin',
-    'material.admin.default',
+    'django.contrib.admin',
+    # 'material.admin',
+    # 'material.admin.default',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.staticfiles',
@@ -172,24 +172,24 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 STATIC_URL = '/static/'
 
-MATERIAL_ADMIN_SITE = {
-    'HEADER':  ('Lakey-service admin panel'),  # Admin site header
-    'TITLE':  ('Lakey-service'),  # Admin site title
-    # 'FAVICON':  'path/to/favicon',  # Admin site favicon (path to static should be specified)
-    # 'MAIN_BG_COLOR':  'black',  # Admin site main color, css color should be specified
-    # 'MAIN_HOVER_COLOR':  'red',  # Admin site main hover color, css color should be specified
-    # 'PROFILE_PICTURE':  'path/to/image',  # Admin site profile picture (path to static should be specified)
-    # 'PROFILE_BG':  'path/to/image',  # Admin site profile background (path to static should be specified)
-    # 'LOGIN_LOGO':  'path/to/image',  # Admin site logo on login page (path to static should be specified)
-    # 'LOGOUT_BG':  'path/to/image',  # Admin site background on login/logout pages (path to static should be specified)
-    'SHOW_THEMES':  True,  #  Show default admin themes button
-    'TRAY_REVERSE': False,  # Hide object-tools and additional-submit-line by default
-    'NAVBAR_REVERSE': False,  # Hide side navbar by default
-    'SHOW_COUNTS': True, # Show instances counts for each model
-    # 'APP_ICONS': {  # Set icons for applications(lowercase), including 3rd party apps, {'application_name': 'material_icon_name', ...}
-    #     'sites': 'send',
-    # },
-    # 'MODEL_ICONS': {  # Set icons for models(lowercase), including 3rd party models, {'model_name': 'material_icon_name', ...}
-    #     'site': 'contact_mail',
-    # }
-}
+# MATERIAL_ADMIN_SITE = {
+#     'HEADER':  ('Lakey-service admin panel'),  # Admin site header
+#     'TITLE':  ('Lakey-service'),  # Admin site title
+#     # 'FAVICON':  'path/to/favicon',  # Admin site favicon (path to static should be specified)
+#     # 'MAIN_BG_COLOR':  'black',  # Admin site main color, css color should be specified
+#     # 'MAIN_HOVER_COLOR':  'red',  # Admin site main hover color, css color should be specified
+#     # 'PROFILE_PICTURE':  'path/to/image',  # Admin site profile picture (path to static should be specified)
+#     # 'PROFILE_BG':  'path/to/image',  # Admin site profile background (path to static should be specified)
+#     # 'LOGIN_LOGO':  'path/to/image',  # Admin site logo on login page (path to static should be specified)
+#     # 'LOGOUT_BG':  'path/to/image',  # Admin site background on login/logout pages (path to static should be specified)
+#     'SHOW_THEMES':  True,  #  Show default admin themes button
+#     'TRAY_REVERSE': False,  # Hide object-tools and additional-submit-line by default
+#     'NAVBAR_REVERSE': False,  # Hide side navbar by default
+#     'SHOW_COUNTS': True, # Show instances counts for each model
+#     # 'APP_ICONS': {  # Set icons for applications(lowercase), including 3rd party apps, {'application_name': 'material_icon_name', ...}
+#     #     'sites': 'send',
+#     # },
+#     # 'MODEL_ICONS': {  # Set icons for models(lowercase), including 3rd party models, {'model_name': 'material_icon_name', ...}
+#     #     'site': 'contact_mail',
+#     # }
+# }
